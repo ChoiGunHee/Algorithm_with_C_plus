dbf1042af98bd0fa4d452fe686953d17b1ef979c
GunHee Choi <godrjsmgl@gmail.com>
2021-03-07 10:21:20 +0000

    Solve Backjoon Step01 01_03

---
+++ Backjoon_step/step_01/back_01_03.cpp
@@ -0,0 +1,26 @@
+/**
+* 2021. 03. 04
+* Creater : Gunhee Choi
+* Problem Number : 	10171
+* Title : 고양이
+* Problem :
+	아래 예제와 같이 고양이를 출력하시오.
+* Input : None
+* Output :
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ 
+**/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+	return 0;
+}
