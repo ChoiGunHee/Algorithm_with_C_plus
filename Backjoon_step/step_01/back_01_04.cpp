6aa9dae329478022ec9a2e7a55e280a04ce601c9
GunHee Choi <godrjsmgl@gmail.com>
2021-03-07 10:25:35 +0000

    Solve Backjoon Step01 01_04

---
+++ Backjoon_step/step_01/back_01_04.cpp
@@ -0,0 +1,28 @@
+/**
+* 2021. 03. 07
+* Creater : Gunhee Choi
+* Problem Number : 	10172
+* Title : 개
+* Problem :
+	아래 예제와 같이 개를 출력하시오.
+* Input : None
+* Output :
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+ 
+**/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+	return 0;
+}
