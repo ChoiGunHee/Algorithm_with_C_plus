1102cddcb1d15d1631ba41d3ccc69d66c42e987f
GunHee Choi <godrjsmgl@gmail.com>
2021-03-31 14:09:44 +0000

    Solve Backjoon Step01 01_10 with C++

--- Backjoon_step/step_01/back_01_10.cpp
+++ Backjoon_step/step_01/back_01_10.cpp
@@ -1,14 +1,37 @@
 /**
-* 2021. 03. 22
+* 2021. 03. 31
 * Creater : Gunhee Choi
-* Problem Number : 	1008
-* Title : A/B
+* Problem Number : 	10430
+* Title : 나머지
 * Problem :
-두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
-* Input : 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-	1 3
-* Output :
-첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
-10-9 이하의 오차를 허용한다는 말은 꼭 소수 9번째 자리까지만 출력하라는 뜻이 아니다.
-	0.33333333333333333333333333333333
+(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+
+(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+
+세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+
+* Input : 첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+    5 8 4
+    
+* Output :첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+    1
+    1
+    0
+    0
 **/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    
+    cin >> A >> B >> C;
+    
+    cout << (A+B)%C << endl;
+    cout << ((A%C) +(B%C))%C << endl;
+    cout << (A*B)%C << endl;
+    cout << ((A%C) * (B%C))%C << endl;
+	
+	return 0;
+}
