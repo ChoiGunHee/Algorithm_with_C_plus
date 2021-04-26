0c3b927b3f83ec0a0270acc9c037c1739a65eb50
GunHee Choi <godrjsmgl@gmail.com>
2021-04-26 11:15:04 +0000

    Solve Backjoon03 03_09 with C++

---
+++ Backjoon_step/step_03/back_03_09.cpp
@@ -0,0 +1,34 @@
+/**
+* 2021. 02. 05
+* Creater : Gunhee Choi
+* Problem Number : 2438
+* Title : 별 찍기 - 1
+* Problem :
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+* Input : 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+	5
+	
+* Output : 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+	*
+	**
+	***
+	****
+	*****
+**/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	
+	cin >> num;
+	
+	for(int i=1; i<=num; i++) {
+		for(int j=0; j<i; j++) {
+			cout<< "*";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
