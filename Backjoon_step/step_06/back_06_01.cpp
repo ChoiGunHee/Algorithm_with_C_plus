26a7bd428da2a8761da982e9178f4ce66cdc8e2e
GunHee Choi <godrjsmgl@gmail.com>
2021-06-27 14:06:57 +0000

    Solve Backjoon Step06 06_01 with C++

--- Backjoon_step/step_06/back_06_01.cpp
+++ Backjoon_step/step_06/back_06_01.cpp
@@ -30,11 +30,16 @@ a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i]
 using namespace std;
 
 long long sum(std::vector<int> &a) {
-
+    long long ans = 0;
+    
+    for(int i=0; i<a.size(); i++) {
+        ans += a.at(i);
+    }
+    
 	return ans;
 }
 
 int main() {
-
+    
 	return 0;
 }
