bed9fcb7219a2ed70734d17b7b94ad5143adec93
GunHee Choi <godrjsmgl@gmail.com>
2021-09-06 11:07:57 +0000

    Solve Backjoon Total Problem 15962 with C++

---
+++ Backjoon_total/Problem_015000/Problem_015900/Problem_0015962.cpp
@@ -0,0 +1,25 @@
+/**
+* 2021. 09. 05
+* Creater : Gunhee Choi
+* Problem Number : 7576
+* Title : 토마토
+
+* Problem :
+부산일과학고등학교는 태어난지 얼마 안 된 킹-갓 과학고등학교다. 우리들을 위해 응원의 한마디를 출력하자.
+
+* Input :
+입력은 없다.
+	
+* Output :
+파이팅!!을 출력한다.
+
+**/
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    cout<<"파이팅!!"<<"\n";
+    
+    return 0;
+}
