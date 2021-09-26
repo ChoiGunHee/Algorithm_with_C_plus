ca4f48c62b6cbe37857029a67d46482e129b5044
GunHee Choi <godrjsmgl@gmail.com>
2021-09-26 05:15:00 +0000

    Solve Backjoon Step08 08_08 with C++

---
+++ Backjoon_step/step_08/back_08_08.cpp
@@ -0,0 +1,86 @@
+/**
+* 2021. 09. 26
+* Creater : Gunhee Choi
+* Problem Number : 10757
+* Title : 큰 수 A+B
+
+* Problem :
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+* Input : 첫째 줄에 A와 B가 주어진다. (0 < A,B < 10^10000)
+
+	9223372036854775807 9223372036854775808
+    
+* Output : 첫째 줄에 A+B를 출력한다.
+
+	18446744073709551615
+    
+**/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void reverse(char * arr, const int len) {
+	int i;
+	char temp;
+	
+	for(i=0; i<len/2; i++) {
+		temp = arr[i];
+		arr[i] = arr[len - i - 1];
+		arr[len - i - 1] = temp;
+	}
+}
+
+int main() {
+	char ch_A[10001] = {0, };
+	char ch_B[10001] = {0, };
+	char result[10002] = {0, };
+	
+	int len_A, len_B;
+	int max_len;
+	int i;
+	int value;
+	int up = 0;
+    
+    cin>>ch_A>>ch_B;
+    
+	len_A = strlen(ch_A);
+	len_B = strlen(ch_B);
+	max_len = len_A > len_B ? len_A : len_B;
+	
+	reverse(ch_A, len_A);
+	reverse(ch_B, len_B);
+	
+	for(i=0; i<=max_len; i++) {
+		
+		value = 0;
+		
+		if( ch_A[i] != 0 ) {
+			value += ch_A[i] - '0';
+		}
+		
+		if( ch_B[i] != 0 ) {
+			value += ch_B[i] - '0';
+		}
+		
+		value += up;
+		
+		if( value >= 10 ) {
+			value -= 10;
+			up = 1;
+		} else {
+			up = 0;
+		}
+		
+		result[i] = value + '0';
+	}
+	
+	if(result[max_len] == '0')
+		result[max_len] = 0;
+	
+	reverse(result, strlen(result));
+	
+    cout<<result<<"\n";
+	return 0;
+}
