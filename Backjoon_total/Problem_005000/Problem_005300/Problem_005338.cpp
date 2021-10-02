1744947052d1fb043e62fac757adad59cfedb4df
GunHee Choi <godrjsmgl@gmail.com>
2021-10-02 22:19:19 +0000

    Solve Backjoon Total Problem 5338 with C++

---
+++ Backjoon_total/Problem_005000/Problem_005300/Problem_005338.cpp
@@ -0,0 +1,34 @@
+/**
+* 2021. 10. 02
+* Creater : Gunhee Choi
+* Problem Number : 5338
+* Title : 마이크로소프트 로고
+
+* Problem :
+마이크로소프트 로고를 예제 출력처럼 출력하는 프로그램을 작성하시오.
+
+* Input :
+
+* Output :
+마이크로소프트 로고를 예제 출력처럼 출력한다.
+
+           _.-;;-._
+    '-..-'|   ||   |
+    '-..-'|_.-;;-._|
+    '-..-'|   ||   |
+    '-..-'|_.-''-._|
+
+**/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "       _.-;;-._" << endl;
+	cout << "'-..-'|   ||   |" << endl;	
+	cout << "'-..-'|_.-;;-._|" << endl;
+	cout << "'-..-'|   ||   |" << endl;
+	cout << "'-..-'|_.-''-._|" << endl;
+
+	return 0;
+}
